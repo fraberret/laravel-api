b8d6bb108d9a5bffca864de94e4d0220cdef20b5
fraberret <f.berretta02@gmail.com>
2024-05-20 18:45:40 +0200

    set seeder to generate projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->words(4, true);
+            $project->slug = Str::of($project->title)->slug('-');
+            $project->description = $faker->text(400);
+            $project->cover_image = $faker->imageUrl(600, 300, 'Project', $project->title);
+            $project->save();
+        }
     }
 }
