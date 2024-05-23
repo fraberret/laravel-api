aeae877cf36cd2c9f2e81f907c3f563ae24e488b
fraberret <f.berretta02@gmail.com>
2024-05-23 18:55:00 +0200

    fix edit old value

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -34,16 +34,17 @@
 
             <div class="mb-3">
                 <label for="type_id" class="form-label">Type</label>
-                <select class="form-select form-select-lg" name="type_id" id="type_id">
-                    <option selected disabled>Select one</option>
-
+                <select class="form-select" name="type_id" id="type_id">
+                    <option selected disabled>Select a type</option>
                     @foreach ($types as $type)
-                        <option value="{{ $type->id }}" {{ $type->id == old('type_id') ? 'selected' : '' }}>
-                            {{ $type->name }}</option>
+                        <option value="{{ $type->id }}"
+                            {{ $type->id == old('type_id', $project->type_id) ? 'selected' : '' }}>{{ $type->name }}
+                        </option>
                     @endforeach
 
                 </select>
             </div>
+
 
             <div class="mb-3">
                 <label for="project_link" class="form-label">Project's link</label>
