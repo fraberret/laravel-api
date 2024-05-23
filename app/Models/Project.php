95c499e72eab2461534a443e16b4ac7d673c10a4
fraberret <f.berretta02@gmail.com>
2024-05-23 17:26:36 +0200

    associate one type for post and many post for type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'description', 'project_link', 'cover_image'];
+    protected $fillable = ['title', 'slug', 'description', 'project_link', 'cover_image', 'type_id'];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
