6c9ab90299e8d0ab42800b803c42e25d4aa50d73
fraberret <f.berretta02@gmail.com>
2024-05-30 16:40:11 +0200

    manage the api routes by controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Project::with('type', 'technologies')->orderByDesc('id')->paginate(12);
+        return response()->json([
+            'success' => true,
+            'posts' => $posts
+        ]);
+    }
 }
